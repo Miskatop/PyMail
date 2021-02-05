fc5f71880f3b728fc971369f83316993a10f1d58
Mikhayil <58036447+Miskatop@users.noreply.github.com>
2021-02-05 10:35:32 +0400

    New parse method and more new futures

--- mail.py
+++ mail.py
@@ -8,11 +8,51 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import os
 
+# Errors
+
+class UndefinedReaderError(Exception):
+	def __init__(self, *args):
+		self.msg = args[0] if len(args) != 0 else None
+
+	def __str__(self):
+		if self.msg:
+			return 'UndefinedReaderError {}'.format(self.msg)
+		else:
+			return 'UndefinedReaderError'
+
+
+class ImapError(Exception):
+	def __init__(self, *args):
+		self.msg = args[0] if len(args) != 0 else None
+
+	def __str__(self):
+		if self.msg:
+			return 'ImapError {}'.format(self.msg)
+		else:
+			return 'ImapError'
+
+
+class SmtpError(Exception):
+	def __init__(self, *args):
+		self.msg = args[0] if len(args) != 0 else None
+
+	def __str__(self):
+		if self.msg:
+			return 'SmtpError {}'.format(self.msg)
+		else:
+			return 'SmtpError'
+
+# Main Code
 class Reciver:
-	def __init__(self, login:str, password:str, imap=None):
-		self.imap = imap if imap else 'imap.{}'.format(login.split('@')[-1])
-		self.connection = imaplib.IMAP4_SSL(self.imap)
+	def __init__(self, login:str, password:str, imap=None, message_load_type=0):
+		self.imap = imap if imap else ('imap.{}'.format(login.split('@')[-1]), 993)
+		try:
+			self.connection = imaplib.IMAP4_SSL(*self.imap)
+		except Exception as e:
+			raise ImapError(e)
+
 		self.connection.login(login, password)
+		self.message_load_type = message_load_type
 
 	def load(self, folder:str='inbox', enc="(RFC822)", count=1):
 		result = []
@@ -22,6 +62,7 @@ class Reciver:
 
 			for i in range(messages, messages-count, -1):
 				current_message = {'message':''}
+				this_type = 0
 				res, msg = self.connection.fetch(str(i), enc)
 				for response in msg:
 					if isinstance(response, tuple):
@@ -41,7 +82,9 @@ class Reciver:
 							current_message['attach'] = []
 							for part in msg.walk():
 								try:
-									current_message['message'] += part.get_payload(decode=True).decode()
+									if self.message_load_type == this_type:
+										current_message['message'] = part.get_payload(decode=True).decode()
+									this_type+=1
 								except:
 									pass
 								current_message['attach'].append([part, str(part.get("Content-Disposition"))])
@@ -63,16 +106,43 @@ class Reciver:
 
 
 class EMail:
+	reader = None
+
+
 	def __init__(self, **kwargs):
 		self._login      = kwargs['login']
 		self._password   = kwargs['password']
 		self._smtp = kwargs['smtp'] if 'smtp' in kwargs else ('smtp.'+self._login.split('@')[-1], 456)
-		self._imap = kwargs['imap'] if 'imap' in kwargs else 'imap.{}'.format(self._login.split('@')[-1])
+		self._imap = kwargs['imap'] if 'imap' in kwargs else ('imap.{}'.format(login.split('@')[-1]), 993)
 
 
-		self._server = root.SMTP_SSL( *self._smtp )
+		try:
+			self._server = root.SMTP_SSL( *self._smtp )
+		except Exception as e:
+			raise SmtpError(e)
+
+		if 'reader' in kwargs and kwargs['reader']:
+			try:
+				self.reader = Reciver(self._login, self._password, imap=self._imap)
+			except Exception as e:
+				raise ImapError(e)
+
 		self._server.login( self._login, self._password )
-		self.reader = Reciver(self._login, self._password, imap=kwargs['imap'] if 'imap' in kwargs else self._imap)
+
+
+	def read(self, *args, **kwargs):
+		if self.reader:
+			return self.reader.load(*args, **kwargs)
+		else:
+			raise UndefinedReaderError('Reader is not Defined')
+
+
+	def download(self, *args, **kwargs):
+		if self.reader:
+			return self.reader.download_attachment(*args, **kwargs)
+		else:
+			raise UndefinedReaderError('Reader is not Defined, Send reader=True as argument of class initializer')
+
 
 	def mail(self, **data):
 		""" Method for sending emils """
@@ -82,7 +152,8 @@ class EMail:
 		msg[ 'From' ] = self._login
 		msg.attach( MIMEText( data['message'] if 'message' in data else "" , 'plain' ) )
 
-		self._server.sendmail( self._login, data['to'], msg.as_string() )
+		return self._server.sendmail( self._login, data['to'], msg.as_string() )
+
 
 	def image(self,**data):
 		""" Method for sending images """
@@ -98,18 +169,23 @@ class EMail:
 		image = MIMEImage(img_data, name=os.path.basename(data['path']))
 		msg.attach(image)
 
-		self._server.sendmail( self._login, data['to'], msg.as_string() )
+		return self._server.sendmail( self._login, data['to'], msg.as_string() )
+
 
 	def bomb(self, **data):
 		""" Method for sending emils """
 		msg = MIMEMultipart()
+		ret = []
 
 		msg[ 'Subject' ] = data['subject']
 		msg[ 'From' ] = self._login
 		msg.attach( MIMEText( data['message'], 'plain' ) )
 
 		for i in range(int(data['count'])):
-			self._server.sendmail( self._login, data['to'], msg.as_string() )
+			res.append(self._server.sendmail( self._login, data['to'], msg.as_string() ))
+
+		return res
+
 
 	def html_message(self, **data):
 		html = data['html']
@@ -120,7 +196,8 @@ class EMail:
 		msg[ 'From' ] = self._login
 		msg.attach( MIMEText( html, 'html' ) )
 
-		self._server.sendmail( self._login, data['to'], msg.as_string() )
+		return self._server.sendmail( self._login, data['to'], msg.as_string() )
+
 
 	def read_file(self, path, encoding="utf-8"):
 		with open(path, 'r', encoding=encoding) as f:
@@ -128,10 +205,9 @@ class EMail:
 
 
 	def stop(self):
-		self._server.quit()
+		return self._server.quit()
 
 if __name__ == '__main__':
 	mail = EMail(login='Your@mail.login', password='yourmailpassword')
 	mail.mail(message="Hello", subject='Simple Html Message', to='reciver@email.addres')
 	mail.stop()
-
